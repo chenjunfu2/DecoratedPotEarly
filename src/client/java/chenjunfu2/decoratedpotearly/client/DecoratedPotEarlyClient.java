3f34e7fa898df5570035a0ff86ab9c66294bfa19
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-06-12 03:18:16 +0800

    1

--- src/client/java/chenjunfu2/decoratedpotearly/client/DecoratedPotEarlyClient.java
+++ src/client/java/chenjunfu2/decoratedpotearly/client/DecoratedPotEarlyClient.java
@@ -1,16 +1,12 @@
 package chenjunfu2.decoratedpotearly.client;
 
-import chenjunfu2.decoratedpotearly.client.registry.DustPlumeParticle;
+import chenjunfu2.decoratedpotearly.client.registry.ClientModParticles;
 import net.fabricmc.api.ClientModInitializer;
 
 public class DecoratedPotEarlyClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient()
 	{
-		ParticleFactoryRegistry.getInstance().register(
-			YourMod.DUST_PLUME_PARTICLE,          // 你自己的 ParticleType
-			DustPlumeParticle.Factory::new        // 直接复用的原版工厂
-		);
-	
+		ClientModParticles.registerParticles();
 	}
 }
