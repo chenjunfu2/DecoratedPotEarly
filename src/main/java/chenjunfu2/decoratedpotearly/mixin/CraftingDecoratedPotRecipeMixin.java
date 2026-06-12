72f6c8ef860b08d14d684967679d752fe833ce1a
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-06-12 17:07:03 +0800

    修复nbt fixxxxxx

--- src/main/java/chenjunfu2/decoratedpotearly/mixin/CraftingDecoratedPotRecipeMixin.java
+++ src/main/java/chenjunfu2/decoratedpotearly/mixin/CraftingDecoratedPotRecipeMixin.java
@@ -2,6 +2,7 @@ package chenjunfu2.decoratedpotearly.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.recipe.CraftingDecoratedPotRecipe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +17,19 @@ public abstract class CraftingDecoratedPotRecipeMixin
 	)
 	private static ItemStack getPotStackWithModifyReturnValue(ItemStack original)
 	{
-		var itemNbt = original.getNbt();
-		if(itemNbt != null)
+		var itemTag = original.getNbt();
+		if(itemTag == null || ! itemTag.contains("BlockEntityTag", NbtElement.COMPOUND_TYPE))
 		{
-			itemNbt.remove("id");
+			return original;
 		}
+		
+		//获取方块实体tag
+		var tagBETag = itemTag.getCompound("BlockEntityTag");
+		
+		//移除无用id
+		tagBETag.remove("id");
+		
+		//因为总是有其它tag，不做后续处理
 		
 		return original;
 	}
